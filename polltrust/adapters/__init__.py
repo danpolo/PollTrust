2781bfc10064b8ff7554fcc27303e9ac8519343c
Dan <37445402+danpolo@users.noreply.github.com>
2026-09-18 02:11:13 +0300

    Export current Wikipedia polling adapter

--- polltrust/adapters/__init__.py
+++ polltrust/adapters/__init__.py
@@ -1,5 +1,11 @@
 from .base import PollSourceAdapter
 from .json_feed import JsonFeedAdapter
 from .repository_fixture import RepositoryFixtureAdapter
+from .wikipedia_2026 import Wikipedia2026PollingAdapter
 
-__all__ = ["PollSourceAdapter", "JsonFeedAdapter", "RepositoryFixtureAdapter"]
+__all__ = [
+    "PollSourceAdapter",
+    "JsonFeedAdapter",
+    "RepositoryFixtureAdapter",
+    "Wikipedia2026PollingAdapter",
+]
